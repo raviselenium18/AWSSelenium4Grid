f1ee2acf7efffefc3b875b2cfa4eb7c59aa4fc45
raviselenium18 <ravi.selenium18@gmail.com>
2022-01-30 17:56:38 +0400

    Framework commit

---
+++ src/test/java/grid/TestRemote.java
@@ -0,0 +1,45 @@
+package grid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class TestRemote {
+
+	//public static void main(String[] args) throws MalformedURLException, InterruptedException {
+	@Test
+	public void googleTest() throws MalformedURLException, InterruptedException {
+		FirefoxOptions opt = new FirefoxOptions();
+
+		WebDriver driver = new RemoteWebDriver(new URL("http://13.127.248.106:4444/"),opt);
+		driver.get("http://google.com");
+		driver.findElement(By.name("q")).sendKeys("Hello Grid!!!");
+		System.out.println("Google");
+		System.out.println("AWS");
+		driver.quit();
+	}
+}
+//public class TestRemote {
+//
+//	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+//
+//
+//		FirefoxOptions opt = new FirefoxOptions();
+//
+//		WebDriver driver = new RemoteWebDriver(new URL("http://13.127.248.106:4444/"),opt);
+//		driver.get("http://google.com");
+//		driver.findElement(By.name("q")).sendKeys("Hello Grid!!!");
+//		System.out.println("Google");
+//		driver.quit();
+//
+//
+//	}
+//
+//}
